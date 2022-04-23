afa9947d6785d45a67406bb3d3d5e2ee56897c16
TTfive <asecondfive@gmail.com>
2022-04-23 20:06:53 +0800

    格式化文档

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#-*- coding=utf-8 -*-
+# -*- coding=utf-8 -*-
 from time import sleep
 import requests
 import sys
@@ -11,8 +11,8 @@ import AES_SECRET
 
 service = None  # 客服
 taskid = None  # 导出表格文件id
-filename =  None # 导出的文件名称
-passwd = None # 文件密码
+filename = None  # 导出的文件名称
+passwd = None  # 文件密码
 kf_name = {
     'hesong-sansan': '丁沪婉',
     'hesong-leixuan': '雷轩',
@@ -20,27 +20,27 @@ kf_name = {
     '鹤松医药008': '兜底组008',
     '鹤松009': '兜底组009',
     '闻毓': '闻毓',
-    'tsurumatsu':'tsurumatsu'
+    'tsurumatsu': 'tsurumatsu'
 }
 # 客服值班表
 kf_zbb = {
-    '2022-04-22':'季雅囡',
-    '2022-04-23':'雷轩',
-    '2022-04-24':'丁沪婉',
-    '2022-04-25':'季雅囡',
-    '2022-04-26':'雷轩',
-    '2022-04-27':'丁沪婉',
-    '2022-04-28':'季雅囡',
-    '2022-04-29':'雷轩',
-    '2022-04-30':'丁沪婉'
+    '2022-04-22': '季雅囡',
+    '2022-04-23': '雷轩',
+    '2022-04-24': '丁沪婉',
+    '2022-04-25': '季雅囡',
+    '2022-04-26': '雷轩',
+    '2022-04-27': '丁沪婉',
+    '2022-04-28': '季雅囡',
+    '2022-04-29': '雷轩',
+    '2022-04-30': '丁沪婉'
 }
-kfs = [] # 客服人员
+kfs = []  # 客服人员
 sale = {
-    '销售额':0,
-    '订单总数':0,
-    '取消订单数':0
-} # 销售情况
-week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+    '销售额': 0,
+    '订单总数': 0,
+    '取消订单数': 0
+}  # 销售情况
+week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
 
 
 def waiterSession(pageSize, startTime, endTime):
@@ -77,7 +77,8 @@ def waiterSession(pageSize, startTime, endTime):
         kfsum = df['service'].value_counts()
         for kf in kfsum.index:
             kfs.append(kf)
-            print("{}接待{}人".format(kf_name[kf],kfsum[kf]))
+            print("{}接待{}人".format(kf_name[kf], kfsum[kf]))
+
 
 def orderDetail(pageSize, startTime, endTime):
     '''客服管家->咚咚查询->促成订单查询
@@ -113,7 +114,8 @@ def orderDetail(pageSize, startTime, endTime):
             df = pd.json_normalize(res['orderDetailList'])
             kfsum = df['service'].value_counts()
             for kf in kfsum.index:
-                print("{}促成{}单".format(kf_name[kf],kfsum[kf]))
+                print("{}促成{}单".format(kf_name[kf], kfsum[kf]))
+
 
 def workload(startTime, endTime, servicePin):
     '''客服管家->客服个人工作数据->工作量（前日一天）
@@ -147,8 +149,8 @@ def workload(startTime, endTime, servicePin):
     if r.status_code == 200:
         r.encoding = 'utf-8'
         res = r.json()
-        print("{}在线时长：{}小时".format(kf_name[servicePin],res['totalDetail']['onlineTime']))
-
+        print("{}在线时长：{}小时".format(
+            kf_name[servicePin], res['totalDetail']['onlineTime']))
 
 
 def doSave(startDate, endDate):
@@ -184,6 +186,7 @@ def doSave(startDate, endDate):
         if(res['success']):
             print("创建导出任务完成！")
 
+
 def list():
     '''导出列表->获取最新的导出文件ID
     https://export.shop.jd.com/exportCenter/list
@@ -215,6 +218,7 @@ def list():
         global taskid
         taskid = res['pageModel']['itemList'][0]['id']
         print("获取最新的导出文件ID:{}".format(taskid))
+
 
 def export(id):
     '''下载导出文件 exportCenter/export.action
@@ -250,11 +254,13 @@ def export(id):
             zfile = zipfile.ZipFile('downloads/{}.zip'.format(id), 'r')
             filename = zfile.namelist()
             print(filename)
-            zfile.extractall('downloads/{}/'.format(id),pwd=passwd.encode('utf-8'))
+            zfile.extractall('downloads/{}/'.format(id),
+                             pwd=passwd.encode('utf-8'))
             print("导出表格文件完成")
-            
+
         except:
             print("解压密码错误！")
+
 
 def sendPassword(id):
     '''发送短信验证码
@@ -287,7 +293,6 @@ def sendPassword(id):
         res = r.json()
         if(res['success']):
             print("发送验证码完成，返回:{}".format(res['data']))
-
 
 
 def getOrderDetail(orderId):
@@ -325,6 +330,7 @@ def getOrderDetail(orderId):
             return ""
     else:
         return ""
+
 
 def phoneSensltiveInfo(orderId, accessKey):
     '''查询手机号码
@@ -380,16 +386,18 @@ def run_kefu_tj():
     orderDetail(100, config.yesterday, config.yesterday)
     sleep(config.duration)
     for kfpin in kfs:
-        workload( config.yesterday, config.yesterday,kfpin)
+        workload(config.yesterday, config.yesterday, kfpin)
         sleep(config.duration)
-    
-    print("今日[{}]勤務中:{}, Doryoku!\n\n".format(week_list[config.today.weekday()], kf_zbb[str(config.today)]))
+
+    print("今日[{}]勤務中:{}, Doryoku!\n\n".format(
+        week_list[config.today.weekday()], kf_zbb[str(config.today)]))
 
 
 def run_dingdan_tj():
     '''每天统计销售情况并获取订单客户手机号码
     '''
-    doSave("{}{}".format(config.yesterday, " 00:00:00"), "{}{}".format(config.yesterday, " 23:59:59"))
+    doSave("{}{}".format(config.yesterday, " 00:00:00"),
+           "{}{}".format(config.yesterday, " 23:59:59"))
     sleep(config.duration)
     list()
     sleep(config.duration)
@@ -397,7 +405,8 @@ def run_dingdan_tj():
     sleep(config.duration)
     export(taskid)
 
-    df = pd.read_csv('./downloads/{}/{}'.format(taskid, filename[0]), encoding = 'gbk')
+    df = pd.read_csv('./downloads/{}/{}'.format(taskid,
+                     filename[0]), encoding='gbk')
     sale['销售额'] = int(df['京东价'].sum())
     sale['订单总数'] = df.shape[0]
     for index, row in df.iterrows():
@@ -405,17 +414,19 @@ def run_dingdan_tj():
             sale['取消订单数'] = sale['取消订单数'] + 1
         print('订单号:', row['订单号'])
         accesskey = getOrderDetail(row['订单号'])
-        print('accesskey:',accesskey)
+        print('accesskey:', accesskey)
         sleep(config.duration)
         phoneNumber = phoneSensltiveInfo(row['订单号'], accesskey)
         print('phoneNumber:', phoneNumber)
         sleep(config.duration)
-        df.loc[index,'联系电话'] = phoneNumber # 更新手机号码
-    df.to_excel('E:/客服销售表/temp/{}_{}排版销售表.xlsx'.format(str(config.yesterday), kf_zbb[str(config.yesterday)]), columns=['订单号','商品ID','商品名称','订购数量','支付方式','下单时间','京东价','订单金额','结算金额','余额支付','应付金额','订单状态','订单类型','下单帐号','客户姓名','客户地址','联系电话','订单备注'])
+        df.loc[index, '联系电话'] = phoneNumber  # 更新手机号码
+    df.to_excel('E:/客服销售表/temp/{}_{}排版销售表.xlsx'.format(str(config.yesterday), kf_zbb[str(config.yesterday)]), columns=[
+                '订单号', '商品ID', '商品名称', '订购数量', '支付方式', '下单时间', '京东价', '订单金额', '结算金额', '余额支付', '应付金额', '订单状态', '订单类型', '下单帐号', '客户姓名', '客户地址', '联系电话', '订单备注'])
     print("\n\n\n昨日[{}]".format(str(config.yesterday)))
-    print('销售额:{}元 订单总数:{} 取消订单:{}'.format(sale['销售额'], sale['订单总数'], sale['取消订单数']))
+    print('销售额:{}元 订单总数:{} 取消订单:{}'.format(
+        sale['销售额'], sale['订单总数'], sale['取消订单数']))
+
 
 if __name__ == '__main__':
     run_dingdan_tj()
     run_kefu_tj()
-
