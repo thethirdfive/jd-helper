67817c9479043093551ac5b883c42123613f1147
TTfive <asecondfive@gmail.com>
2022-04-23 20:16:04 +0800

    去除多余测试文件，更新readme说明

--- test.py
+++
@@ -1,9 +0,0 @@
-from AES_SECRET import AES_ENCRYPT
-
-
-if __name__ == '__main__':
-    aes_encrypt = AES_ENCRYPT()
-    mobile = 'Y7yUMqjFLhHW5sALAwsidA=='
-    phone = "13585096000"
-
-    print(aes_encrypt.decrypt(mobile))
